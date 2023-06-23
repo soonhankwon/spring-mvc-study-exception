99c59a0630f8e43db0c775f0de85694b0ded20a4
soonable@naver.com <soonable@naver.com>
2023-06-23 17:41:41 +0900

    feat : API Exception - @ControllerAdvice 컨트롤러 별로 적용

--- src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
+++ src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -1,5 +1,6 @@
 package hello.exception.exhandler.advice;
 
+import hello.exception.api.ApiExceptionV3Controller;
 import hello.exception.exception.UserException;
 import hello.exception.exhandler.ErrorResult;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = {ApiExceptionV3Controller.class})
 public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
